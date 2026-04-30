2d7d549b63e35878328c8619c940311111b47a7b
Clau <claudia_molina@ceroca.cat>
2026-04-30 10:57:17 +0200

    colore

---
+++ lib/Colors_app.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static var bar = const Color.fromRGBO(19, 25, 33, 1);
+  static var menu = const Color.fromRGBO(35, 47, 62, 1);
+  static var search = const Color.fromRGBO(254, 189, 105, 1);
+}
