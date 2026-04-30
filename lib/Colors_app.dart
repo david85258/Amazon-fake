d5ea3cad9b46ec7b14ed304dead229d7ae436246
Clau <claudia_molina@ceroca.cat>
2026-04-30 11:02:14 +0200

    colo

--- lib/Colors_app.dart
+++ lib/Colors_app.dart
@@ -5,4 +5,5 @@ class ColorsApp {
   static var bar = const Color.fromRGBO(19, 25, 33, 1);
   static var menu = const Color.fromRGBO(35, 47, 62, 1);
   static var search = const Color.fromRGBO(254, 189, 105, 1);
+  static var elements = const Color.fromRGBO(230, 230, 230, 1);
 }
