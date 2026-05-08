2332037ecd3103c0e97129d6336f86e0bcc6f8e7
david85258 <77178948+david85258@users.noreply.github.com>
2026-05-08 13:56:39 +0200

    "Footer"

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -76,7 +76,7 @@ class MainPage extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 50,),
+            SizedBox(height: 50),
             Container(
               color: Colors.white,
               padding: const EdgeInsets.all(25.0),
@@ -86,12 +86,21 @@ class MainPage extends StatelessWidget {
                 children: [
                   Row(
                     children: [
-                      Text("Ofertas en Hogar y jardín", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
-                      SizedBox(width: 20,),
-                      Text("Descubre mas", style: TextStyle(color: Colors.blue, fontSize: 14),)
+                      Text(
+                        "Ofertas en Hogar y jardín",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Text(
+                        "Descubre mas",
+                        style: TextStyle(color: Colors.blue, fontSize: 14),
+                      ),
                     ],
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(height: 20),
                   GridView.count(
                     crossAxisCount: 7,
                     shrinkWrap: true,
@@ -120,12 +129,21 @@ class MainPage extends StatelessWidget {
                 children: [
                   Row(
                     children: [
-                      Text("Ofertas en Hogar y jardín Parte 2", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
-                      SizedBox(width: 20,),
-                      Text("Compra ahora", style: TextStyle(color: Colors.blue, fontSize: 14),)
+                      Text(
+                        "Ofertas en Hogar y jardín Parte 2",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Text(
+                        "Compra ahora",
+                        style: TextStyle(color: Colors.blue, fontSize: 14),
+                      ),
                     ],
                   ),
-                  SizedBox(height: 20,),
+                  SizedBox(height: 20),
                   GridView.count(
                     crossAxisCount: 7,
                     shrinkWrap: true,
@@ -146,40 +164,79 @@ class MainPage extends StatelessWidget {
               ),
             ),
             Padding(
-                  padding: const EdgeInsets.all(25.0),
-                  child: GridView.count(
-                    crossAxisCount: 4,
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    crossAxisSpacing: 50,
-                    mainAxisSpacing: 20,
-                    children: [
-                      Ofertas(
-                        texto: "Oferta Top",
-                        imagen: "lib/images/productos1.png",
-                        peque: "",
-                      ),
-                      Ofertas(
-                        texto: "Ofertas en Outlet",
-                        imagen: "lib/images/productos2.png",
-                        peque: "Explorar ahora",
-                      ),
-                      Ofertas(
-                        texto: "Hogar y cocina",
-                        imagen: "lib/images/productos3.png",
-                        peque: "Saber mas",
-                      ),
-                      Ofertas(
-                        texto: "Informatica y accesorios",
-                        imagen: "lib/images/productos4.png",
-                        peque: "Descubre mas",
-                      )
-                    ],
+              padding: const EdgeInsets.all(25.0),
+              child: GridView.count(
+                crossAxisCount: 4,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisSpacing: 50,
+                mainAxisSpacing: 20,
+                children: [
+                  Ofertas(
+                    texto: "Oferta Top",
+                    imagen: "lib/images/productos1.png",
+                    peque: "",
                   ),
+                  Ofertas(
+                    texto: "Ofertas en Outlet",
+                    imagen: "lib/images/productos2.png",
+                    peque: "Explorar ahora",
+                  ),
+                  Ofertas(
+                    texto: "Hogar y cocina",
+                    imagen: "lib/images/productos3.png",
+                    peque: "Saber mas",
+                  ),
+                  Ofertas(
+                    texto: "Informatica y accesorios",
+                    imagen: "lib/images/productos4.png",
+                    peque: "Descubre mas",
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 50),
+            Container(
+              color: Colors.white,
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 25.0, top: 25.0),
+                padding: const EdgeInsets.only(bottom: 20.0, top: 30.0),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.5, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
                 ),
+                child: Column(
+                  children: [
+                    Text("Ver recomendaciones personalizadas", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+                    SizedBox(height: 10),
+                    Container(
+                      width: 200,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Center(child: Text("Identificarse", style: TextStyle(fontWeight: FontWeight.bold),),),
+                    ),
+                    SizedBox(height: 5,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("¿Eres un cliente nuevo? ", style: TextStyle(fontSize: 10),),
+                        Text("Empieza aquí", style: TextStyle(fontSize: 10,color: Colors.blue, decoration: TextDecoration.underline, decorationColor: Colors.blue),)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              color: ColorsApp.menu,
+              child: Center(child: Text("Volver arriba", style: TextStyle(color: Colors.white),),),
+            )
           ],
         ),
-        
       ),
     );
   }
