47edc3cbea1030f8e4856729696ca7a6f7c6d153
david85258 <77178948+david85258@users.noreply.github.com>
2026-05-08 10:36:42 +0200

    asad

--- lib/components/ofertas.dart
+++ lib/components/ofertas.dart
@@ -4,23 +4,23 @@ class Ofertas extends StatelessWidget {
   final String texto;
   final String imagen;
   final String peque;
+
   const Ofertas({super.key, required this.texto, required this.imagen, required this.peque});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      width: 200,
+      width: 250,
       height: 250,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(texto, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+          Text(texto, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
           Center(child: Image.asset(imagen, height: 180,)),
-          Text(peque, style: TextStyle(color: Colors.blue, fontSize: 13),)
+          Text(peque, style: TextStyle(color: Colors.blue, fontSize: 12),)
         ],
       ),
     );
-    
   }
 }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -13,15 +13,23 @@ class MainPage extends StatelessWidget {
           children: [
             UpperBar(),
             Image.asset("lib/images/fondo.png"),
-            Ofertas(texto: "Oferta Top",imagen: "lib/images/productos1.png",peque: "",),
-            Ofertas(texto: "Ofertas en Outlet",imagen: "lib/images/productos2.png",peque: "Explorar ahora",),
-            Ofertas(texto: "Hogar y cocina",imagen: "lib/images/productos3.png",peque: "Saber mas",),
-            Ofertas(texto: "Informatica y accesorios",imagen: "lib/images/productos4.png",peque: "Descubre mas",),
-            Ofertas(texto: "Regalos que le haran sonreir",imagen: "lib/images/productos5.png",peque: "Comprar ahora",),
-            Ofertas(texto: "Los libros mas vendidos",imagen: "lib/images/productos6.png",peque: "Ver mas",),
-            Ofertas(texto: "La tienda del Dia de la Madre",imagen: "lib/images/productos7.png",peque: "Comprar ahora",),
-            Ofertas(texto: "Ofertas para ti",imagen: "lib/images/productos8.png",peque: "Explora ahora",),
-
+            GridView.count(
+              crossAxisCount: 4,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              crossAxisSpacing: 0,
+              mainAxisSpacing: 10,
+              children: [
+                Ofertas(texto: "Oferta Top",imagen: "lib/images/productos1.png",peque: "",),
+                Ofertas(texto: "Ofertas en Outlet",imagen: "lib/images/productos2.png",peque: "Explorar ahora",),
+                Ofertas(texto: "Hogar y cocina",imagen: "lib/images/productos3.png",peque: "Saber mas",),
+                Ofertas(texto: "Informatica y accesorios",imagen: "lib/images/productos4.png",peque: "Descubre mas",),
+                Ofertas(texto: "Regalos que le haran sonreir",imagen: "lib/images/productos5.png",peque: "Comprar ahora",),
+                Ofertas(texto: "Los libros mas vendidos",imagen: "lib/images/productos6.png",peque: "Ver mas",),
+                Ofertas(texto: "La tienda del Dia de la Madre",imagen: "lib/images/productos7.png",peque: "Comprar ahora",),
+                Ofertas(texto: "Ofertas para ti",imagen: "lib/images/productos8.png",peque: "Explora ahora",),
+              ],
+            )
           ],
         ),
       ),
